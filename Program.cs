145dc30e13502ba2276f1e36c9813f0fe454e113
syedfaizz <syedfaizz45@gmail.com>
2021-08-12 17:55:52 +0530

    [faisal] Add: welcome

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool rs = User.PasswordR4();
+            Console.WriteLine(rs);
+        }
+    }
+}
