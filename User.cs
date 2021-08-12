97fc0bfeec5f9591acb36774897e63162a46aab1
syedfaizz <syedfaizz45@gmail.com>
2021-08-12 18:07:10 +0530

    [faisal] Add: uc1-user-first-name

---
+++ User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class User
+    {
+        public const string FIRST_NAME = "^[A-Z]{1}[a-z]{2}";
+       
+        public static bool ValidateFirstName()
+        {
+            Regex regex = new Regex(FIRST_NAME);
+            return regex.IsMatch("Faisal");
+        }
+    }
+}
