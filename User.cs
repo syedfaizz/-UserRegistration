a562a6af39baee36b13f892f33432f1e7b2e73d2
syedfaizz <syedfaizz45@gmail.com>
2021-08-12 18:36:41 +0530

    [faisal] Add: uc3-user-email

--- User.cs
+++ User.cs
@@ -7,8 +7,9 @@ namespace UserRegistration
 {
     class User
     {
-        public const string FIRST_NAME = "^[A-Z]{1}[a-z]{2}";
-        public const string LAST_NAME = "^[A-Z]{1}[a-z]{2}";
+        public const string FIRST_NAME = "^[A-Z][a-zA-Z]{2}";
+        public const string LAST_NAME = "^[A-Z][a-zA-Z]{2}";
+        public const string EMAIL = "^[a-zA-z]{3}([.]*[a-zA-Z])+[@][a-zA-z]+([.][a-z]{2,3})*$";
         public static bool ValidateFirstName()
         {
             Regex regex = new Regex(FIRST_NAME);
@@ -18,6 +19,11 @@ namespace UserRegistration
         {
             Regex regex = new Regex(LAST_NAME);
             return regex.IsMatch("Sayed");
+        }
+        public static bool ValidateEmail()
+        {
+            Regex regex = new Regex(EMAIL);
+            return regex.IsMatch("abc.xyz@bl.co.in");
         }
     }
 }
