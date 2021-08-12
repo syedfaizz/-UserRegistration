cfa733639529c703f31a92eade9a5416358208bc
syedfaizz <syedfaizz45@gmail.com>
2021-08-12 19:19:40 +0530

    [faisal] Add: uc4

--- User.cs
+++ User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -10,20 +11,27 @@ namespace UserRegistration
         public const string FIRST_NAME = "^[A-Z][a-zA-Z]{2}";
         public const string LAST_NAME = "^[A-Z][a-zA-Z]{2}";
         public const string EMAIL = "^[a-zA-z]{3}([.]*[a-zA-Z])+[@][a-zA-z]+([.][a-z]{2,3})*$";
+        public const string MOBILE = "^([9][1])+[ ]+[789]{1}[0-9]{9}$";
         public static bool ValidateFirstName()
         {
             Regex regex = new Regex(FIRST_NAME);
-            return regex.IsMatch("Faisal");
+            return regex.IsMatch("fasial");
         }
         public static bool ValidateLastName()
         {
             Regex regex = new Regex(LAST_NAME);
-            return regex.IsMatch("Sayed");
+            return regex.IsMatch("KUMAR");
         }
         public static bool ValidateEmail()
         {
             Regex regex = new Regex(EMAIL);
             return regex.IsMatch("abc.xyz@bl.co.in");
+        }
+        public static
+            bool ValidatePhone()
+        {
+            Regex regex = new Regex(MOBILE);
+            return regex.IsMatch("91 8967904554");
         }
     }
 }
