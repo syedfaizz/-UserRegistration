ff48c868adf87e6c17c2d599ca3b5c80bb75b8b4
syedfaizz <syedfaizz45@gmail.com>
2021-08-12 18:16:32 +0530

    [faisal] Add: uc2-user-last-name

--- User.cs
+++ User.cs
@@ -8,11 +8,16 @@ namespace UserRegistration
     class User
     {
         public const string FIRST_NAME = "^[A-Z]{1}[a-z]{2}";
-       
+        public const string LAST_NAME = "^[A-Z]{1}[a-z]{2}";
         public static bool ValidateFirstName()
         {
             Regex regex = new Regex(FIRST_NAME);
             return regex.IsMatch("Faisal");
+        }
+        public static bool ValidateLastName()
+        {
+            Regex regex = new Regex(LAST_NAME);
+            return regex.IsMatch("Sayed");
         }
     }
 }
